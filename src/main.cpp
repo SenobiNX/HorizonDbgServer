edb1aa2b093f47e07a8ceb5e0259383b2e61bd75
fruityloops1 <fruityloops@littun.co>
2025-08-25 00:16:44 +0200

    faggot

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "hk/services/socket/config.h"
 #include "hk/services/socket/service.h"
 #include "hk/types.h"
+#include "hk/util/Tuple.h"
+#include "hk/util/TypeName.h"
 
 constexpr hk::socket::ServiceConfig cConfig;
 alignas(hk::cPageSize) u8 buffer[cConfig.calcTransferMemorySize() + 0x20000];
@@ -15,28 +17,24 @@ extern "C" void hkMain() {
 
     auto* socket = hk::socket::Socket::initialize(cConfig, buffer);
 
-    {
-        /*auto logger = HK_UNWRAP(hk::lm::LogManager::initialize()->getLogger());
-
-    logger.log("loger");
-
-    HK_ABORT("asdfsd", 0);*/
-    }
-
     auto [err, errno] = socket->socket(hk::socket::AddressFamily::Ipv4, hk::socket::Type::Stream, hk::socket::Protocol(0));
 
     s32 fd = err;
     HK_ABORT_UNLESS(fd >= 0, "fd: %d, errno: %d", fd, errno);
 
-    tie(err, errno) = socket->bind(fd, hk::socket::SocketAddrIpv4::parse<"0.0.0.0">(2405));
+    tie(err, errno) = socket->bind(fd, hk::socket::SocketAddrIpv4::parse<"0.0.0.0">(8008));
     HK_ABORT_UNLESS(err >= 0, "err: %d, errno: %d", err, errno);
 
-    hk::socket::SocketAddrIpv4 client;
-    tie(err, errno, client) = socket->accept(fd);
+    tie(err, errno) = socket->listen(fd, 1);
     HK_ABORT_UNLESS(err >= 0, "err: %d, errno: %d", err, errno);
+
+    s32 clientFd;
+    hk::socket::SocketAddrIpv4 client;
+    tie(clientFd, errno, client) = socket->accept(fd);
+    HK_ABORT_UNLESS(clientFd >= 0, "clientFd: %d, errno: %d", clientFd, errno);
 
     constexpr auto shit = "PENIS!";
-    tie(err, errno) = socket->send(fd, std::span<const char>(shit, sizeof("PENIS!")), 0);
+    tie(err, errno) = socket->send(clientFd, std::span<const char>(shit, sizeof("PENIS!")), 0);
 
     HK_ABORT("bye %d %d", err, errno);
 }
